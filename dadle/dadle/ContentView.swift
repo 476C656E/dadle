31e1c25d505004e5ea34ba6e8e0163ef91615df7
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-09-04 02:05:19 +0900

    add SplashView

--- dadle/dadle/ContentView.swift
+++ dadle/dadle/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+    ZStack{
+        Color.blue
+            .ignoresSafeArea()
         Text("Hello, world!")
+            .foregroundColor(.white)
             .padding()
+        }
     }
 }
 
---
+++ dadle/dadle/SplashView.swift
@@ -0,0 +1,38 @@
+//
+//  SplashView.swift
+//  dadle
+//
+//  Created by 강민규 on 2022/09/04.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    
+    @State var isActiveBool = false
+    
+    var body: some View {
+        VStack {
+            if self.isActiveBool {
+                ContentView()
+            } else {
+                Text("Splash")
+                
+            }
+        }.onAppear{
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3){
+                withAnimation {
+                    self.isActiveBool = true
+                }
+            }
+        }
+        
+        
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
