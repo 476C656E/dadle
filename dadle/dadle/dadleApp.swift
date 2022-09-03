e96d29d1fd766df8cf504e3c3d3d1efb96249740
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-09-04 01:54:21 +0900

    project init

---
+++ dadle/dadle/dadleApp.swift
@@ -0,0 +1,17 @@
+//
+//  dadleApp.swift
+//  dadle
+//
+//  Created by 강민규 on 2022/09/04.
+//
+
+import SwiftUI
+
+@main
+struct dadleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
