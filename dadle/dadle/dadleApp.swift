413986c0a49145ccecae83f47bd1d1764932a958
476C656E <476c656e@gmail.com>
2022-09-06 02:23:37 +0900

    Firebase 및 delegate 등록

--- dadle/dadle/dadleApp.swift
+++ dadle/dadle/dadleApp.swift
@@ -7,17 +7,31 @@
 
 import SwiftUI
 
+import FirebaseCore
 import KakaoSDKAuth
 import KakaoSDKCommon
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 @main
 struct dadleApp: App {
+    // 파이어베이스 셋업 앱 딜리게이트 등록
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     init() {
         // Kakao SDK Init
         KakaoSDK.initSDK(appKey: "298f3f8f42259cd80cd4c11099aee74b")
     }
     var body: some Scene {
         WindowGroup {
+            // onOpenURL()을 사용해 커스텀 URL 스킴 처리
             OnboardingView()
                 .onOpenURL { url in
                     if (AuthApi.isKakaoTalkLoginUrl(url)) {
