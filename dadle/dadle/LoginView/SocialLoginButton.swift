2c18318c0668960da0a8c0f9119fb7e8fe41f886
476C656E <476c656e@gmail.com>
2022-09-11 10:48:49 +0900

    Add to Assets and LoginBtnView

--- dadle/dadle/LoginView/SocialLoginButton.swift
+++ dadle/dadle/LoginView/SocialLoginButton.swift
@@ -7,12 +7,49 @@
 
 import SwiftUI
 
+// MARK: Kakao Login Button Style
+struct KakaoLoginButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 300, height: 25)
+            .padding()
+            .background(Color(red:0, green: 0, blue: 0.5))
+            .foregroundColor(.white)
+            .cornerRadius(5)
+            .scaleEffect(configuration.isPressed ? 1.1 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
+// MARK: Apple Login Button Style
+struct AppleLoginButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 300, height: 25)
+            .padding()
+            .background(Color(red:0, green: 0.5, blue: 0))
+            .foregroundColor(.white)
+            .cornerRadius(5)
+            .shadow(color: .black, radius: 3, x: 0, y: 1)
+            .scaleEffect(configuration.isPressed ? 1.1 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
 struct SocialLoginButton: View {
     var body: some View {
-        Button {
-            // Kakao SDK Action
-        } label : {
-            Text("kakao Login")
+        VStack  {
+            
+            Button("Kakao Login") {
+                // Kakao SDKAction
+                print("Kakako Btn Pressed!")
+            }
+            .buttonStyle(KakaoLoginButtonStyle())
+            
+            Button("Apple Login") {
+                // Apple SDKAction
+                print("Apple Btn pressed!")
+            }.buttonStyle(AppleLoginButtonStyle())
         }
     }
 }
